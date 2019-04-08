228ff47b142cf340fb69e7f48dda2365bbcf2528
Unsar Malik <unsar.malik24@gmail.com>
2019-04-08 03:01:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,8 +21,8 @@ def remove_element_from_end_of_array(array)
   arrays = array.pop
 end
 def remove_element_from_start_of_array(array)
-  array = ["I","am", "really", "learning", "arrays!"]
-  I = array.shift
+  array = ["wow","I","am", "really", "learning", "arrays!"]
+  wow = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
